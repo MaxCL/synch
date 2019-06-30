facd48a980a65d28ead58bd28e722a1c7373828e
MaxCL <mecontrl@uc.cl>
2019-06-30 13:04:35 +0200

    Better description for local functions.

--- UnimodalFunctions.c
+++ UnimodalFunctions.c
@@ -1,13 +1,20 @@
+
 /************************************************************************/
-/* Name: UnimodalFunctions.c						*/
-/* Description: Implementation of Unimodal Functions			*/
-/* Author: Max Contreras						*/
-/* Date: June 7 2016							*/
+// File Name: UnimodalFunctions.c
+// Description: Local functions for a 1D lattice:
+// Author: Max Contreras - mecontrl@uc.cl
+// Date: July 2016 @ Complex Systems PUC Rio - Brazil
 /************************************************************************/
 
 #include "UnimodalFunctions.h"
 
-// Logistic map
+/************************************************************************/
+// Function: Logistic map
+// Input: parameter and current state
+// Return: double
+//  Description: Calculates the next iteration for a logistic map
+/************************************************************************/
+
 double UF_logmap(double r, double x)
 {
 	double y = 0.0;
@@ -17,7 +24,13 @@ double UF_logmap(double r, double x)
 	return y;
 }
 
-// Sine map
+/************************************************************************/
+// Function: Sine map
+// Input: parameter and current state
+// Return: double
+//  Description: Calculates the next iteration for a sine map
+/************************************************************************/
+
 double UF_sin(double r, double x)
 {
 	double y = 0.0;
@@ -27,7 +40,13 @@ double UF_sin(double r, double x)
 	return y;
 }
 
-// Quadratic map 
+/************************************************************************/
+// Function: quadratic map
+// Input: parameter and current state
+// Return: double
+//  Description: Calculates the next iteration for a quadratic map
+/************************************************************************/
+
 double UF_qmap(double r, double x)
 {
 	double y = 0.0;
