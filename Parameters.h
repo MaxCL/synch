dd7410390a4bc9ad0c9081ebba60ad591b530c5a
MaxCL <mecontrl@uc.cl>
2019-07-01 00:10:15 +0200

    Inclusion of global FACTOR for random numbers.

---
+++ Parameters.h
@@ -0,0 +1,23 @@
+
+/************************************************************************/
+// File Name: Parameters.h
+// Description: General parameters for the simulations
+// Author: Max Contreras - mecontrl@uc.cl
+// Date: July 2016 @ Complex Systems PUC Rio - Brazil
+/************************************************************************/
+
+// Number of elements in the network
+#define NODES 201
+
+// Number of iterations: Transient 10,000
+#define NUMBER_OF_ITERATIONS 10201
+
+// Parameter for the logistic map - chaotic state
+#define MU 4.00
+
+// Initial diference between the original orbit and the
+// shadow orbit used to calculate the Lyapunov exponent
+#define DELTA 0.00000001
+
+// Factor for random numbers precision
+#define FACTOR 100.0
