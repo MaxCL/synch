c230fe5ab562a35ba6468e705b24b65ba379022e
MaxCL <mecontrl@uc.cl>
2019-08-24 19:04:33 +0200

    Program to generate cases for a single unit with multiple cores.

--- gen_process_list.c
+++ gen_process_list.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+  int number_of_cores;
+
   int alpha;
   double d_alpha;
 
@@ -12,6 +14,23 @@ int main(void)
   int gamma;
   double d_gamma;
 
+  FILE *file_01;
+
+  file_01 = fopen("cases_list","w+");
+  if( file_01 == NULL)
+  {
+    printf("FILE cases_list ERROR \n");
+    exit(1);
+  }
+
+  if( argc != 2 )
+	{
+		printf("Use 1 input parameter: number of cores\n");
+		exit(1);
+	}
+
+  sscanf(argv[1],"%d",&number_of_cores);
+
   for( alpha = 0 ; alpha <= 300 ; alpha++)
   {
     for( epsilon = 0 ; epsilon <= 100 ; epsilon++)
@@ -20,10 +39,10 @@ int main(void)
       d_epsilon = (double)epsilon/100;
       d_gamma = 0.0;
 
-      printf("./synch %1.2lf %1.2lf %1.2lf &\n", d_alpha, d_epsilon, d_gamma);
-      if( (epsilon != 0) && (epsilon%7 == 0) )
+      fprintf(file_01,"./synch %1.2lf %1.2lf %1.2lf &\n", d_alpha, d_epsilon, d_gamma);
+      if( (epsilon != 0) && (epsilon%number_of_cores == 0) )
       {
-        printf("wait\n");
+        fprintf(file_01,"wait\n");
       }
 
     }
